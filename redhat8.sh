fe411660002bad220ef6d430fa9faf33c1d6f3ef
Tanakit Intaniyom <imTanakit@gmail.com>
2024-06-04 13:27:44 +0700

    initial commit

---
+++ redhat8.sh
@@ -0,0 +1,13 @@
+#1 inactive user
+output=$(useradd -D | grep INACTIVE | cut -d'=' -f 2)
+echo $output
+
+#2 minlen
+#/etc/pam.d/system-auth - pam_cracklib.so - minlen"
+output=$(egrep '^password\s+requisite\s+pam_cracklib.so' /etc/pam.d/system-auth | awk '{for(i=1; i<=NF; i++) {tmp=match($i, /minlen/); if(tmp) { print $i } } }' | cut -d'=' -f 2)
+echo $output
+
+#3 user expiry
+username=$USER
+chage -l $username 
+
